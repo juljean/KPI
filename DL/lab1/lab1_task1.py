740a8ee9eeb215035d988510a782d45a3b01a164
yuliia <yuliia.nad@g-experts.net>
2022-10-10 14:57:00 +0200

    Ready task 1 from lab 1

---
+++ DL/lab1/lab1_task1.py
@@ -0,0 +1,57 @@
+import math
+import random
+
+
+class Neuron:
+    def __init__(self, x, yr, dd, y_act=lambda x: 1 / (1 + math.exp(-x))):
+        """
+        Initialization of parameters in class
+        :param x: list of float numbers, input signals
+        :param yr: expected output value
+        :param dd: float, allowed error
+        :param y_act: activation function
+        """
+        self.x = x
+        # Number of signals
+        self.x_num = len(x)
+        self.w = [random.random() * 0.01 for i in range(self.x_num)]
+        self.dd = dd
+        self.yr = yr
+        self.y_act = y_act
+
+    def learn(self):
+        """
+        Learning mode
+        :return: list of float numbers, normalized weights
+        """
+        xs = 0
+        for ind in range(self.x_num):
+            xs += self.x[ind] * self.w[ind]
+        y = self.y_act(xs)
+        # current error for calculated y
+        dn = abs((self.yr - y) / self.yr)
+        print("y: ", y, "dn:", dn)
+        if dn > self.dd:
+            # weights normalization
+            q = y * (1 - y) * (self.yr - y)
+            dw = [self.x[ind] * q for ind in range(self.x_num)]
+            self.w = [self.w[ind] + dw[ind] for ind in range(self.x_num)]
+            return self.learn()
+        return self.w
+
+    def recognition(self):
+        """
+        Recognition mode
+        :return:
+        """
+        xs = 0
+        for n in range(self.x_num):
+            xs += eval(f"x" + str(n)) * eval(f"self.w" + str(n))
+        y = self.y_act(xs)
+        dn = abs((self.yr - y) / self.yr)
+        print("y: ", y, "dn:", dn)
+
+
+neur = Neuron([1, 3, 5, 7], 0.3, 0.1)
+print("Optimized weights:", *(neur.learn()))
+
