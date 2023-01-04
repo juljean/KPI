d7aedffb00c3590d2562535ee4b6669268c0e78e
yuliia <yuliia.nad@g-experts.net>
2023-01-04 19:10:18 +0100

    Ready Lab3 ASKM

--- ASKM/Lab3.py
+++ ASKM/Lab3.py
@@ -36,41 +36,41 @@ def rk4_2(x, y, y_, h=0.05):
     k2_ = h * dif_eq(x + h / 2, y + k1 / 2, y_ + k1_ / 2)
     k3 = h * F(x + 2 * h / 3, y + k0 - 2 * k1 + 2 * k2, y_ + k0_ - 2 * k1_ + 2 * k2_)
     k3_ = h * dif_eq(x + 2 * h / 3, y + k0 - 2 * k1 + 2 * k2, y_ + k0_ - 2 * k1_ + 2 * k2_)
-    y = y + (k0 + 2 * k1 + 2 * k2 + k3) / 6
-    y_ = y_ + (k0_ + 2 * k1_ + 2 * k2_ + k3_) / 6
+    y = y + (k0 + 4 * k2 + k3) / 6
+    y_ = y_ + (k0_ + 4 * k2_ + k3_) / 6
     return y, y_
 
 
 def rk3(x, y, y_, h=0.05):
     k0 = h * F(x, y, y_)
-    l0 = h * dif_eq(x, y, y_)
-    k1 = h * F(x + h / 2, y + k0 / 2, y_ + l0 / 2)
-    l1 = h * dif_eq(x + h / 2, y + k0 / 2, y_ + l0 / 2)
-    k2 = h * F(x + h, y - k0 + 2 * k1, y_ - l0 + 2 * l1)
-    l2 = h * dif_eq(x + h, y - k0 + 2 * k1, y_ - l0 + 2 * l1)
-    y = y + (k0 + 4 * k1 + k2) / 6
-    y_ = y_ + (l0 + 4 * l1 + l2) / 6
+    k0_ = h * dif_eq(x, y, y_)
+    k1 = h * F(x + h / 2, y + k0 / 2, y_ + k0_ / 2)
+    k1_ = h * dif_eq(x + h / 2, y + k0 / 2, y_ + k0_ / 2)
+    k2 = h * F(x + h, y - k0 + 2 * k1, y_ - k0_ + 2 * k1_)
+    k2_ = h * dif_eq(x + h, y - k0 + 2 * k1, y_ - k0_ + 2 * k1_)
+    y = y + (k0 + 4*k1 + k2)/6
+    y_ = y_ + (k0_ + 4*k1_ + k2_)/6
 
     return y, y_
 
 
 def rk2_1(x, y, y_, h=0.05):
     k0 = h * F(x, y, y_)
-    l0 = h * dif_eq(x, y, y_)
-    k1 = h * F(x + h, y + k0, y_ + l0 / 2)
-    l1 = h * dif_eq(x + h, y + k0, y_ + l0 / 2)
+    k0_ = h * dif_eq(x, y, y_)
+    k1 = h * F(x + h, y + k0, y_ + k0_ / 2)
+    k1_ = h * dif_eq(x + h, y + k0, y_ + k0_ / 2)
     y = y + (k0 + k1) / 2
-    dy = y_ + (l0 + l1) / 2
+    dy = y_ + (k0_ + k1_) / 2
     return y, dy
 
 
 def rk2_2(x, y, y_, h=0.05):
     k0 = h * F(x, y, y_)
-    l0 = h * dif_eq(x, y, y_)
-    k1 = h * F(x + h / 2, y + k0 / 2, y_ + l0 / 2)
-    l1 = h * dif_eq(x + h / 2, y + k0 / 2, y_ + l0 / 2)
+    k0_ = h * dif_eq(x, y, y_)
+    k1 = h * F(x + h / 2, y + k0 / 2, y_ + k0_ / 2)
+    k1_ = h * dif_eq(x + h / 2, y + k0 / 2, y_ + k0_ / 2)
     y = y + (k0 + k1) / 2
-    dy = y_ + (l0 + l1) / 2
+    dy = y_ + (k0_ + k1_) / 2
     return y, dy
 
 
